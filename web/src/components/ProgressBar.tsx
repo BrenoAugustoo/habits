da6d7c12595f438d5d494336fd3cf808f86764f8
Breno Augusto <brenoaugusto98@gmail.com>
2023-01-23 19:28:02 -0300

    Aula 04: Front-end

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,21 @@
+interface ProgressBarProps {
+  progress: number
+}
+
+export function ProgressBar ({progress}: ProgressBarProps) {
+  const progressStyls = {
+    width: `${progress}%`
+  }
+
+  return (
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4" >
+      <div 
+        className="h-3 rounded-xl bg-violet-600"
+        aria-label="Progresso de hábitos completados nesse dia"
+        aria-valuenow={progress}
+        role="progressbar"
+        style={progressStyls}
+      />
+    </div>
+  )
+}
