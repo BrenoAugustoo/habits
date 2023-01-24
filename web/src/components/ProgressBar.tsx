fe9a62f9d15775272a5e44c84d2c8327f30c6360
Breno Augusto <brenoaugusto98@gmail.com>
2023-01-23 22:22:45 -0300

    finalizando aplicação

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -10,7 +10,7 @@ export function ProgressBar ({progress}: ProgressBarProps) {
   return (
     <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4" >
       <div 
-        className="h-3 rounded-xl bg-violet-600"
+        className="h-3 rounded-xl bg-violet-600 transition-all"
         aria-label="Progresso de hábitos completados nesse dia"
         aria-valuenow={progress}
         role="progressbar"
